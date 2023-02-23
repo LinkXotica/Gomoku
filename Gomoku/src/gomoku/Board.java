2592f309cbde0c7986221e03106b5b27cd40ffc7
LinkXotica <124554620+LinkXotica@users.noreply.github.com>
2023-02-22 19:22:40 -0700

    switch to desktop

--- Gomoku/src/gomoku/Board.java
+++ Gomoku/src/gomoku/Board.java
@@ -5,12 +5,13 @@ public class Board {
 	// Input X,Y = [y][x]
 	
 	//Attributes *****************
-	private Place [][] board = new Place[15][15];
+	private Place [][] board;
 	private Player player1;
 	private Player player2;
 	
 	public Board (int size, String playerType1, String playerType2) {
 		int [] x = new int[2];
+		board = new Place[size][size];
 		for(int i = 0; i < size; i ++) {
 			for(int j = 0; j < size; j ++) {
 				x[0] = i;
@@ -19,14 +20,25 @@ public class Board {
 			}
 		}
 		if(playerType1 == "Human") {
-			this.player1 = new HumanPlayer();
+			this.player1 = new HumanPlayer(true);
 		} else {
-			this.player1 = new ComPlayer();
+			this.player1 = new ComPlayer(true);
+		}
+		
+		if(playerType2 == "Human") {
+			this.player2 = new HumanPlayer(false);
+		} else {
+			this.player2 = new ComPlayer(false);
 		}
 	}
 	
-	public Board() {
-		this(15);
+	public void switchTurn() {
+		player1.changeTurn();
+		player2.changeTurn();
+	}
+	
+	public Board(String playerType1, String playerType2) {
+		this(15, playerType1, playerType2);
 	}
 	
 	
